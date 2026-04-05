813d733959e1b09d179ba6d4ca3b3d104f14b27e
tksh21-githab <tksh21+githab@gmail.com>
2026-04-05 16:31:45 +0900

    update: 2026-04-05 16:31

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Obsidianのpublicacionesからコピーしてpush
+
+SRC="$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notas/*Personal/tela nuestra/publicaciones/"
+DEST="$HOME/Desktop/tela-nuestra/content/publicaciones/"
+
+mkdir -p "$DEST"
+rsync -av --delete "$SRC" "$DEST"
+
+cd "$HOME/Desktop/tela-nuestra"
+git add .
+git commit -m "update: $(date '+%Y-%m-%d %H:%M')"
+git push
